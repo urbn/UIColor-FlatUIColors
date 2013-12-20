b201d6e4123370e24239145e7cc99b58edeb5ab3
ksteel <ksteelman2@gmail.com>
2013-12-20 12:24:39 -0500

    moved to source file

---
+++ Source/UIColor+FlatUIColors.h
@@ -0,0 +1,34 @@
+//
+//  UIColor+FlatUIColors.h
+//  Source: http://flatuicolors.com/
+//
+//  Created by Rex Finn on 12/13/13.
+//  Copyright (c) 2013 Rex Finn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (FlatUIColors)
+
++ (instancetype)FUAlizarinColor;
++ (instancetype)FUAmethystColor;
++ (instancetype)FUAsbestosColor;
++ (instancetype)FUBelizeHoleColor;
++ (instancetype)FUCarrotColor;
++ (instancetype)FUCloudsColor;
++ (instancetype)FUConcreteColor;
++ (instancetype)FUEmeraldColor;
++ (instancetype)FUGreenSeaColor;
++ (instancetype)FUMidnightBlueColor;
++ (instancetype)FUNephritisColor;
++ (instancetype)FUFlatOrangeColor;
++ (instancetype)FUPeterRiverColor;
++ (instancetype)FUPomegranateColor;
++ (instancetype)FUPumpkinColor;
++ (instancetype)FUSilverColor;
++ (instancetype)FUSunFlowerColor;
++ (instancetype)FUTurquoiseColor;
++ (instancetype)FUWetAsphaltColor;
++ (instancetype)FUWisteriaColor;
+
+@end
---
+++ Source/UIColor+FlatUIColors.m
@@ -0,0 +1,93 @@
+//
+//  UIColor+FlatUIColors.m
+//  Source: http://flatuicolors.com/
+//
+//  Created by Rex Finn on 12/13/13.
+//  Copyright (c) 2013 Rex Finn. All rights reserved.
+//
+
+#import "UIColor+FlatUIColors.h"
+
+@implementation UIColor (FlatUIColors)
+
++ (instancetype)FUAlizarinColor {
+    return [UIColor colorWithRed:0.906 green:0.298 blue:0.235 alpha:1.00];
+}
+
++ (instancetype)FUAmethystColor {
+    return [UIColor colorWithRed:0.608 green:0.349 blue:0.714 alpha:1.00];
+}
+
++ (instancetype)FUAsbestosColor {
+    return [UIColor colorWithRed:0.498 green:0.549 blue:0.553 alpha:1.00];
+}
+
++ (instancetype)FUBelizeHoleColor {
+    return [UIColor colorWithRed:0.161 green:0.502 blue:0.725 alpha:1.00];
+}
+
++ (instancetype)FUCarrotColor {
+    return [UIColor colorWithRed:0.902 green:0.494 blue:0.133 alpha:1.00];
+}
+
++ (instancetype)FUCloudsColor {
+    return [UIColor colorWithRed:0.925 green:0.941 blue:0.945 alpha:1.00];
+}
+
++ (instancetype)FUConcreteColor {
+    return [UIColor colorWithRed:0.584 green:0.647 blue:0.651 alpha:1.00];
+}
+
++ (instancetype)FUEmeraldColor {
+    return [UIColor colorWithRed:0.180 green:0.800 blue:0.443 alpha:1.00];
+}
+
++ (instancetype)FUGreenSeaColor {
+    return [UIColor colorWithRed:0.086 green:0.627 blue:0.522 alpha:1.00];
+}
+
++ (instancetype)FUMidnightBlueColor {
+    return [UIColor colorWithRed:0.173 green:0.243 blue:0.314 alpha:1.00];
+}
+
++ (instancetype)FUNephritisColor {
+    return [UIColor colorWithRed:0.153 green:0.682 blue:0.376 alpha:1.00];
+}
+
++ (instancetype)FUFlatOrangeColor {
+    return [UIColor colorWithRed:0.953 green:0.612 blue:0.071 alpha:1.00];
+}
+
++ (instancetype)FUPeterRiverColor {
+    return [UIColor colorWithRed:0.204 green:0.596 blue:0.859 alpha:1.00];
+}
+
++ (instancetype)FUPomegranateColor {
+    return [UIColor colorWithRed:0.753 green:0.224 blue:0.169 alpha:1.00];
+}
+
++ (instancetype)FUPumpkinColor {
+    return [UIColor colorWithRed:0.827 green:0.329 blue:0.000 alpha:1.00];
+}
+
++ (instancetype)FUSilverColor {
+    return [UIColor colorWithRed:0.741 green:0.765 blue:0.780 alpha:1.00];
+}
+
++ (instancetype)FUSunFlowerColor {
+    return [UIColor colorWithRed:0.945 green:0.769 blue:0.059 alpha:1.00];
+}
+
++ (instancetype)FUTurquoiseColor {
+    return [UIColor colorWithRed:0.102 green:0.737 blue:0.612 alpha:1.00];
+}
+
++ (instancetype)FUWetAsphaltColor {
+    return [UIColor colorWithRed:0.204 green:0.286 blue:0.369 alpha:1.00];
+}
+
++ (instancetype)FUWisteriaColor {
+    return [UIColor colorWithRed:0.557 green:0.267 blue:0.678 alpha:1.00];
+}
+
+@end
